9195533c920be5677f6bf65fa7c7787d941e4493
Connor Shea <connor.james.shea@gmail.com>
2016-10-06 23:37:38 -0600

    Add a filter to replace .md URLs with .html, this fixes a ton of URLs.

---
+++ lib/filters/markdown_to_html_ext.rb
@@ -0,0 +1,16 @@
+module Nanoc::Filters
+  class MarkdownToHtmlExt < Nanoc::Filter
+    identifier :md_to_html_ext
+
+    # Convert internal URLs that link to `.md` files to instead link to
+    #{ }`.html` files since that's what Nanoc actually serves.
+    def run(content, params = {})
+      content.gsub(/href="(\S*.md)"/) do |result| # Fetch all links in the HTML Document
+        if /http/.match(result).nil? # Check if link is internal
+          result.gsub!(/\.md/, '.html') # Replace the extension if link is internal
+        end
+        result
+      end
+    end
+  end
+end
