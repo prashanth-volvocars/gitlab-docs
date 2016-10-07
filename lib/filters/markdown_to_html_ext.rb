9305670f16d296ea7690e6d7e4cff9d2ddbb76ca
Achilleas Pipinellis <axilleas@axilleas.me>
2016-10-07 11:20:07 +0200

    Extend the regex to capture anchor links as well

--- lib/filters/markdown_to_html_ext.rb
+++ lib/filters/markdown_to_html_ext.rb
@@ -5,7 +5,7 @@ module Nanoc::Filters
     # Convert internal URLs that link to `.md` files to instead link to
     #{ }`.html` files since that's what Nanoc actually serves.
     def run(content, params = {})
-      content.gsub(/href="(\S*.md)"/) do |result| # Fetch all links in the HTML Document
+      content.gsub(/href="(\S*.md\S*)"/) do |result| # Fetch all links in the HTML Document
         if /http/.match(result).nil? # Check if link is internal
           result.gsub!(/\.md/, '.html') # Replace the extension if link is internal
         end
