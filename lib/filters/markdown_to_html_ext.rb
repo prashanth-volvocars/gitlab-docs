10684f9c2e85b1a6553bca2ad8e2137745b46bdf
Connor Shea <connor.james.shea@gmail.com>
2016-11-09 16:17:14 -0700

    Improve the matcher for markdown links.

--- lib/filters/markdown_to_html_ext.rb
+++ lib/filters/markdown_to_html_ext.rb
@@ -6,7 +6,7 @@ module Nanoc::Filters
     #{ }`.html` files since that's what Nanoc actually serves.
     def run(content, params = {})
       content.gsub(/href="(\S*.md\S*)"/) do |result| # Fetch all links in the HTML Document
-        if /http/.match(result).nil? # Check if link is internal
+        if /^href="http/.match(result).nil? # Check if link is internal
           result.gsub!(/\.md/, '.html') # Replace the extension if link is internal
         end
         result
