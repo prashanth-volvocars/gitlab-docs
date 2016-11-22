b43087fb68934db1c9f992b9d580562742ef7dc0
Connor Shea <connor.james.shea@gmail.com>
2016-11-22 15:46:54 -0700

    Add anchor links to all headers.

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -9,4 +9,10 @@ require 'rouge/plugins/redcarpet'
 
 class HTML < Redcarpet::Render::HTML
   include Rouge::Plugins::Redcarpet
+
+  def header(text, header_level)
+    anchor = text.downcase.strip.split(" ").join("-")
+
+    "<h#{header_level} id='#{anchor}'>#{text} <a class='anchor' href='##{anchor}' title='Permalink'>&para;</a></h#{header_level}>"
+  end
 end
