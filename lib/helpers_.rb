6bfa43c1e487befb1a113e498fc6fa17cd658382
Achilleas Pipinellis <axilleas@axilleas.me>
2016-11-23 12:48:09 +0100

    Replace punctuation with dash

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -16,7 +16,7 @@ class HTML < Redcarpet::Render::HTML
   def header(text, header_level)
     # https://gitlab.com/gitlab-org/gitlab-ce/blob/0676c5c7140ccf5b809eddab79b6fb78b7db0a66/lib/banzai/filter/table_of_contents_filter.rb#L29-32
     anchor = text.downcase
-    anchor.gsub!(PUNCTUATION_REGEXP, '') # remove punctuation
+    anchor.gsub!(PUNCTUATION_REGEXP, '-') # replace punctuation with dash
     anchor.tr!(' ', '-') # replace spaces with dash
     anchor.squeeze!('-') # replace multiple dashes with one
 
