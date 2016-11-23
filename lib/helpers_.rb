a91ad32346b4da89196e8f7395c6cb3f480f6085
Achilleas Pipinellis <axilleas@axilleas.me>
2016-11-23 12:35:17 +0100

    Generate anchor links the way Banzai does

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -11,6 +11,18 @@ require 'rouge/plugins/redcarpet'
 class HTML < Redcarpet::Render::HTML
   include Rouge::Plugins::Redcarpet
 
+  PUNCTUATION_REGEXP = /[^\p{Word}\- ]/u
+
+  def header(text, header_level)
+    # https://gitlab.com/gitlab-org/gitlab-ce/blob/0676c5c7140ccf5b809eddab79b6fb78b7db0a66/lib/banzai/filter/table_of_contents_filter.rb#L29-32
+    anchor = text.downcase
+    anchor.gsub!(PUNCTUATION_REGEXP, '') # remove punctuation
+    anchor.tr!(' ', '-') # replace spaces with dash
+    anchor.squeeze!('-') # replace multiple dashes with one
+
+    "<h#{header_level} id='#{anchor}'>#{text} <a class='anchor' href='##{anchor}' title='Permalink'>&para;</a></h#{header_level}>"
+  end
+
   def image(link, title, alt_text)
     %(<a target="_blank" href="#{link}"><img src="#{link}" title="#{title}" alt="#{alt_text}"/></a>)
   end
