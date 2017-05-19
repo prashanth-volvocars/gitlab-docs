012b9fb3bed492946c490bda8a30d4c7481b99c7
Sean Packham (GitLab) <sean@seanpackham.com>
2017-05-19 13:09:16 +0000

    Tweaks to layouts and styles

--- content/assets/javascripts/docs.js
+++ content/assets/javascripts/docs.js
@@ -19,18 +19,39 @@ function toggleNavigation() {
     var sidebar = document.getElementById('doc-nav');
 
     if(sidebar) {
-      sidebar.appendChild(nav[0]);
+      // if there is one h1 in the documentation
+      if(nav[0].children.length == 1) {
+
+        // if there is a nested ul after the first anchor
+        if(nav[0].children[0].children.length > 1) {
+          var menu = nav[0].children[0].children[1];
+
+          // grab the h1's li anchor text
+          var title = document.createElement('h4');
+          title.innerHTML = nav[0].children[0].children[0].innerHTML;
+
+          // add the text as a title
+          menu.insertBefore(title, menu.children[0]);
+
+          sidebar.appendChild(menu);
+        }
+
+        // remove what is left of the old navigation
+        nav[0].remove()
+      }
+      else {
+        nav[0].remove()
+      }
     }
 
-
-    // float the main content
+    // main content has-toc
     var main = document.querySelectorAll('.main.class');
 
     if (main[0] && main[0].classList) {
-      main[0].classList.add('float-left');
+      main[0].classList.add('has-toc');
     }
     else {
-      main[0].className += ' float-left';
+      main[0].className += ' has-toc';
     }
   }
 })();
