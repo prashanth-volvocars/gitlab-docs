ae32b71aaca0c024d7fabfb81850626b042a548a
Marcia Ramos <virtua.creative@gmail.com>
2018-04-17 16:51:38 +0000

    Add badges to docs

---
+++ content/assets/javascripts/badges.js
@@ -0,0 +1,95 @@
+(function() {
+  var BADGES_TITLES = {
+    core: 'Available in GitLab Core, GitLab.com Free, and higher tiers',
+    starter: 'Available in GitLab Starter, GitLab.com Bronze, and higher tiers',
+    premium: 'Available in GitLab Premium, GitLab.com Silver, and higher tiers',
+    ultimate: 'Available in GitLab Ultimate and GitLab.com Gold',
+    'core-only': 'Available in GitLab Core and higher tiers. Not available in GitLab.com',
+    'starter-only': 'Available in GitLab Starter and higher tiers. Not available in GitLab.com',
+    'premium-only': 'Available in GitLab Premium and higher tiers. Not available in GitLab.com',
+    'ultimate-only': 'Available in GitLab Ultimate. Not available in GitLab.com',
+  };
+
+  var BADGES_MAPPING = {
+    core: ['core', 'free'],
+    starter: ['starter', 'bronze'],
+    premium: ['premium', 'silver'],
+    ultimate: ['ultimate', 'gold'],
+    'core-only': ['core'],
+    'starter-only': ['starter'],
+    'premium-only': ['premium'],
+    'ultimate-only': ['ultimate']
+  };
+
+  var BADGES_CLASS = {
+    core: 'gitlab',
+    starter: 'gitlab',
+    premium: 'gitlab',
+    ultimate: 'gitlab',
+    free: 'gitlab-com',
+    bronze: 'gitlab-com',
+    silver: 'gitlab-com',
+    gold: 'gitlab-com',
+  };
+
+  function init() {
+    var $badges = $('.badge-trigger');
+
+    $badges.each(function() {
+      convertBadge($(this));
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+
+  function convertBadge($badge) {
+    var small = isSmall($badge);
+    var badgeType = retrieveBadgeType($badge);
+
+    var smallBadgeTag = function() {
+      return '<span class="badge-small"><i class="fa fa-question-circle-o" aria-hidden="true"></i></span>';
+    };
+
+    var largeBadgeTag = function(badge, badgeClass) {
+      return $('<div>', { class: 'badge-display badge-' + badgeClass, text: badge});
+    };
+
+    var template = function(title, badges) {
+      var container = $('<div>', { class: 'badges-drop', 'data-toggle': 'tooltip', 'data-placement': 'left auto', title: title });
+      container.append($('<span>').append(badges));
+
+      return container;
+    };
+
+    var tags = [];
+
+    if (small) {
+      tags.push(smallBadgeTag());
+    } else {
+      $.each(BADGES_MAPPING[badgeType], function(i, badge) {
+        tags.push(largeBadgeTag(badge, BADGES_CLASS[badge]));
+      });
+    }
+
+    $badge.append($(template(BADGES_TITLES[badgeType], tags)));
+  }
+
+  // Get the badge type from a specific list of expected values in element class
+  function retrieveBadgeType($badge) {
+    var match = $badge.attr('class').match(/core-only|core|starter-only|premium-only|ultimate-only|starter|premium|ultimate/);
+
+    if (match) {
+      return match.pop();
+    }
+  }
+
+  // When badge is not in a HTML header, we use the small version
+  function isSmall($badge) {
+    return !$badge.parent().prop('tagName').match(/H1|H2|H3|H4|H5/);
+  }
+
+  $(function () {
+    init();
+  });
+
+})();
---
+++ lib/filters/badges.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# GitLab price / tiers badge
+#
+# This allows us to add visual Badges to our documentation using standard Markdown
+# that will render in any markdown editor.
+#
+# The available pattern is `**[<BADGE_TYPE> <MODIFIER>]**`
+# The following TIERS are supported: CORE, STARTER, PREMIUM, ULTIMATE
+# The following MODIFIERS are supported: ONLY
+#
+# When you have ONLY as MODIFIER, it means, it applies only for on premise instances
+# so we are not going to expand "STARTER" to "BRONZE" as well.
+class BadgesFilter < Nanoc::Filter
+  identifier :badges
+
+  BADGES_PATTERN = %r{
+    (?:^|[^`]) # must be start of the line or anything except backtick
+    \*\*\[
+    (?<badge_type>CORE|STARTER|PREMIUM|ULTIMATE)(?:\s+(?<only>ONLY))
+    ?\]\*\*
+    (?:$|[^`]) # must end of line or anything except backtick
+  }x
+
+  def run(content, params = {})
+    content.gsub(BADGES_PATTERN) { generate(Regexp.last_match[:badge_type].downcase, !Regexp.last_match[:only].nil?) }
+  end
+
+  def generate(badge_type, only)
+    if only
+      %[<span class="badge-trigger #{badge_type}-only"></span>]
+    else
+      %[<span class="badge-trigger #{badge_type}"></span>]
+    end
+  end
+end
