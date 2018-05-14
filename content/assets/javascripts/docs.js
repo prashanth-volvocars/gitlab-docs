cf95b8b98681d9981b42cad0a2bfa0a9ed94806e
Jarek Ostrowski <jarek.j.ostrowski@gmail.com>
2018-05-14 10:04:06 -0400

    Add conditionals for landing functions to prevent errors

--- content/assets/javascripts/docs.js
+++ content/assets/javascripts/docs.js
@@ -3,20 +3,22 @@ var NAV_INLINE_BREAKPOINT = 1100;
 var landingHeaderBar = document.getElementById('landing-header-bar')
 var headerLinks = document.getElementsByClassName('header-link')
 
-window.addEventListener('scroll', function() {
-  if (window.scrollY >= 100) {
-    landingHeaderBar.classList.add('scrolling-header')
-    for (var i = 0; i < headerLinks.length; i++) {
-      headerLinks[i].classList.add('scrolling-header-links')
+if (landingHeaderBar) {
+  window.addEventListener('scroll', function() {
+    if (window.scrollY >= 100) {
+      landingHeaderBar.classList.add('scrolling-header')
+      for (var i = 0; i < headerLinks.length; i++) {
+        headerLinks[i].classList.add('scrolling-header-links')
+      }
     }
-  }
-  else {
-    landingHeaderBar.classList.remove('scrolling-header')
-    for (var i = 0; i < headerLinks.length; i++) {
-      headerLinks[i].classList.remove('scrolling-header-links')
+    else {
+      landingHeaderBar.classList.remove('scrolling-header')
+      for (var i = 0; i < headerLinks.length; i++) {
+        headerLinks[i].classList.remove('scrolling-header-links')
+      }
     }
-  }
-});
+  });
+}
 
 var navtoggle = document.getElementById("docs-nav-toggle");
 if (navtoggle) {
@@ -36,19 +38,21 @@ function toggleNavigation() {
 
   // Set timeofday var depending on the time //
 
-  var date = new Date()
-  var hour = date.getHours()
+  if (timeofday) {
+    var date = new Date()
+    var hour = date.getHours()
 
-  if (hour < 11) {
-    timeofday.innerHTML = "morning"
-  }
+    if (hour < 11) {
+      timeofday.innerHTML = "morning"
+    }
 
-  if (hour >= 11 && hour < 16) {
-    timeofday.innerHTML = "afternoon"
-  }
+    if (hour >= 11 && hour < 16) {
+      timeofday.innerHTML = "afternoon"
+    }
 
-  if (hour >= 16) {
-    timeofday.innerHTML = "evening"
+    if (hour >= 16) {
+      timeofday.innerHTML = "evening"
+    }
   }
 
   // if the document has a top level nav
