4d6bfdd4e6dda0fc26a1a9dd32f6e4f38cbc527a
Rémy Coutable <remy@rymai.me>
2018-08-20 14:06:49 +0200

    Make the 'Edit in GitLab' link target gitlab-ce when the file exists there
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/helpers/edit_on_gitlab.rb
+++ lib/helpers/edit_on_gitlab.rb
@@ -9,6 +9,7 @@ module Nanoc::Helpers
       product = content_filename_array.shift
       # This should be the path from the doc/ directory for a given file.
       docs_content_filename = content_filename_array.join("/")
+      root_dir = File.expand_path('../../', __dir__)
 
       if product == "omnibus"
         # omnibus-gitlab repo
@@ -16,8 +17,12 @@ module Nanoc::Helpers
       elsif product == "runner"
         # gitlab-runner repo
         gitlab_url = "https://gitlab.com/gitlab-org/gitlab-#{product}/blob/master/docs/#{docs_content_filename}"
-      elsif (product == "ce" or product == "ee")
+      elsif %w[ce ee].include?(product)
         # gitlab-ce and gitlab-ee repos
+        if product == "ee"
+          ce_file = File.join(root_dir, @item[:content_filename].sub(@config[:products][:ee][:dirs][:dest_dir], @config[:products][:ce][:dirs][:dest_dir]))
+          product = "ce" if File.exists?(ce_file)
+        end
         gitlab_url = "https://gitlab.com/gitlab-org/gitlab-#{product}/blob/master/doc/#{docs_content_filename}"
       else
         # gitlab-docs pages
