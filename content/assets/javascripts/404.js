de77c316134edb5ddda6629d44cffe4ee4fb5249
Phil Hughes <me@iamphill.com>
2018-10-26 09:08:00 +0100

    Remove version from smart search

--- content/assets/javascripts/404.js
+++ content/assets/javascripts/404.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var el = document.getElementById('search-input');
   var path = window.location.pathname
+    .replace(/^\/((\d+)\.(\d+)?)(\/?(ee|ce)?)\//, '')
     .split(/[-/_]/)
     .join(' ')
     .trim()
