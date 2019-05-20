b09ee838e77695c5b577271666a490e2412b0afd
Achilleas Pipinellis <axil@gitlab.com>
2019-05-20 23:23:50 +0000

    Add option to link to Web IDE to edit files

--- lib/helpers/edit_on_gitlab.rb
+++ lib/helpers/edit_on_gitlab.rb
@@ -1,6 +1,6 @@
 module Nanoc::Helpers
   module EditOnGitLab
-    def edit_on_gitlab(item)
+    def edit_on_gitlab(item, editor: :simple)
       # Make an array out of the content's source path.
       content_filename_array = @item[:content_filename].split('/')
       # Remove "/content/"
@@ -14,12 +14,15 @@ module Nanoc::Helpers
       if product == "omnibus"
         # omnibus-gitlab repo
         gitlab_url = "https://gitlab.com/gitlab-org/#{product}-gitlab/blob/master/doc/#{docs_content_filename}"
+        gitlab_ide_url = "https://gitlab.com/-/ide/project/gitlab-org/#{product}-gitlab/edit/master/-/doc/#{docs_content_filename}"
       elsif product == "runner"
         # gitlab-runner repo
         gitlab_url = "https://gitlab.com/gitlab-org/gitlab-#{product}/blob/master/docs/#{docs_content_filename}"
+        gitlab_ide_url = "https://gitlab.com/-/ide/project/gitlab-org/gitlab-#{product}/edit/master/-/docs/#{docs_content_filename}"
       elsif product == "charts"
         # GitLab Helm chart repo
         gitlab_url = "https://gitlab.com/#{product}/gitlab/blob/master/doc/#{docs_content_filename}"
+        gitlab_ide_url = "https://gitlab.com/-/ide/project/#{product}/gitlab/edit/master/-/doc/#{docs_content_filename}"
       elsif %w[ce ee].include?(product)
         # gitlab-ce and gitlab-ee repos
         if product == "ee"
@@ -27,14 +30,24 @@ module Nanoc::Helpers
           product = "ce" if File.exists?(ce_file)
         end
         gitlab_url = "https://gitlab.com/gitlab-org/gitlab-#{product}/blob/master/doc/#{docs_content_filename}"
+        gitlab_ide_url = "https://gitlab.com/-/ide/project/gitlab-org/gitlab-#{product}/edit/master/-/doc/#{docs_content_filename}"
       elsif product == "debug"
         gitlab_url = "https://gitlab.com/debugging/#{product}/blob/master/content/#{docs_content_filename}"
+        gitlab_ide_url = "https://gitlab.com/-/ide/project/debugging/#{product}/edit/master/-/content/#{docs_content_filename}"
       else
         # gitlab-docs pages
         gitlab_url = "https://gitlab.com/gitlab-com/gitlab-docs/blob/master/#{@item[:content_filename]}"
+        gitlab_ide_url = "https://gitlab.com/-/ide/project/gitlab-com/gitlab-docs/edit/master/-/#{@item[:content_filename]}"
       end
 
-      result = gitlab_url
+      case editor
+      when :simple
+        gitlab_url
+      when :webide
+        gitlab_ide_url
+      else
+        raise "Unknown editor: #{editor}"
+      end
     end
   end
 end
