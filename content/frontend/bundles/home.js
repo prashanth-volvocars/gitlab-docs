ae65d26c3f21a9614f792c06d09f70dd32e4d955
Jacques Erasmus <jerasmus@gitlab.com>
2019-11-06 07:27:24 +0000

    Add landing page burger menu
    Added a burger menu to the landing page

---
+++ content/frontend/bundles/home.js
@@ -0,0 +1,8 @@
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const navBar = document.getElementById('landing-header-bar');
+    const navToggle = document.getElementById('docs-nav-toggle');
+    navToggle.addEventListener('click', () => navBar.classList.toggle('active'));
+  }
+);
