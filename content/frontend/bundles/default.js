1fda2494ea177d0ee690e7726db1e2a9e02bd7c7
Jacques Erasmus <jerasmus@gitlab.com>
2019-11-15 10:33:33 +0000

    Display version banner for outdated docs
    Added the ability to display a version banner for outdated docs

--- content/frontend/bundles/default.js
+++ content/frontend/bundles/default.js
@@ -1,9 +1,26 @@
 import Vue from 'vue';
 import NavigationToggle from '../components/navigation_toggle/navigation_toggle.vue';
+import VersionBanner from '../components/version_banner/version_banner.vue';
 
 document.addEventListener(
   'DOMContentLoaded',
   () => {
+    const versionBanner = document.querySelector('#js-version-banner');
+    const isOutdated = versionBanner.hasAttribute('data-is-outdated');
+    const { latestVersionUrl, archivesUrl } = versionBanner.dataset;
+
+    new Vue({
+      el: versionBanner,
+      components: {
+        VersionBanner,
+      },
+      render(createElement) {
+        return createElement(VersionBanner, {
+          props: { isOutdated, latestVersionUrl, archivesUrl }
+        });
+      },
+    });
+
     new Vue({
       el: '#js-nav-toggle',
       components: {
@@ -16,6 +33,6 @@ document.addEventListener(
           }
         });
       },
-    })
+    });
   }
 );
---
+++ content/frontend/components/version_banner/version_banner.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    isOutdated: {
+      type: Boolean,
+      required: true,
+    },
+    latestVersionUrl: {
+      type: String,
+      required: true,
+    },
+    archivesUrl: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-if="isOutdated" class="version-banner position-fixed w-100 text-center">
+    This is <a :href="archivesUrl">archived documentation</a> for GitLab. Go to <a :href="latestVersionUrl">the latest</a>.
+  </div>
+</template>
