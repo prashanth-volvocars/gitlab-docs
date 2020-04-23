ed726181bb19eceda1640ee581dd38d1b430feaa
Paul Slaughter <pslaughter@gitlab.com>
2020-04-23 16:42:50 -0500

    Restyle TOC page navigation
    - Flattens nested ul's
    - Styles the nav-links

--- content/assets/javascripts/docs.js
+++ content/assets/javascripts/docs.js
@@ -1,5 +1,5 @@
 ---
-version: 1
+version: 2
 ---
 
 var NAV_INLINE_BREAKPOINT = 1100;
@@ -33,11 +33,29 @@ function toggleNavigation() {
   nav.classList.toggle('active');
 }
 
+function getAncestorsMatching(el, root, fn, count = 0) {
+  if (!el || el === root) {
+    return count;
+  } else {
+    const nextCount = fn(el) ? count + 1 : count;
+
+    return getAncestorsMatching(el.parentElement, root, fn, nextCount);
+  }
+}
+
+function isUlElement(el) {
+  return el && el.tagName === 'UL';  
+}
+
+function getUlAncestorsCount(el, root) {
+  return getAncestorsMatching(el, root, isUlElement);
+}
+
 // move document nav to sidebar
 (function() {
   var timeofday = document.getElementById('timeofday');
-  var tocList = document.querySelector('.js-article-content > ul#markdown-toc');
-  var main = document.querySelector('.js-main-wrapper');
+  var menu = document.querySelector('.js-article-content > ul#markdown-toc');
+  var main = document.querySelector('.js-main-wrapper')
 
   // Set timeofday var depending on the time //
 
@@ -59,24 +77,42 @@ function toggleNavigation() {
   }
 
   // if the document has a top level nav
-  if (tocList) {
+  if (menu) {
     // append to the sidebar
     var sidebar = document.getElementById('doc-nav');
+    var sidebarContentHeight = 0;
+    var sidebarContent = document.createElement('div');
+    sidebarContent.id = 'doc-nav-content';
+    sidebarContent.classList.add('doc-nav-content');
 
     if (sidebar) {
       // if there are items
-      if (tocList.children.length >= 1) {
-        var menu = tocList;
-        $(tocList).addClass('nav nav-pills flex-column');
-        $(tocList).find('ul').addClass('nav nav-pills flex-column');
-        $(tocList).find('a').addClass('nav-link');
+      if (menu.children.length >= 1) {
+        $(menu).addClass('nav nav-pills flex-column indigo-border-nav');
+        $(menu).find('a').addClass('nav-link');
+        $(menu).find('ul').addClass('nav nav-pills flex-column');
+        // We're about to flatten the nested li's so let's add paddings to the nav links
+        menu.querySelectorAll('li li .nav-link').forEach(navLink => {
+          const nestingLevel = getUlAncestorsCount(navLink, menu);
+          const padding = nestingLevel * 25;
+          navLink.style.paddingLeft = `${padding}px`;
+        });
+        // Add all nested li's as top-level children
+        menu.querySelectorAll('li').forEach(li => {
+          menu.appendChild(li);
+        });
+        // Remove ul's which have been emptied now
+        menu.querySelectorAll('ul').forEach(ul => {
+          ul.remove();
+        });
 
         // grab the h1's li anchor text
         var title = document.createElement('h4');
-        title.innerHTML = 'On this page:';
+        title.classList.add('border-bottom-0', 'font-weight-bold', 'mb-0', 'pb-0');
+        title.innerHTML = 'On this page';
 
         // add the text as a title
-        menu.insertBefore(title, menu.children[0]);
+        sidebarContent.appendChild(title);
 
         var hasHelpSection = document.getElementById('help-and-feedback');
 
@@ -94,11 +130,10 @@ function toggleNavigation() {
           menu.insertBefore(separator, menu.children[menu.children.length]);
           menu.insertBefore(listItem, menu.children[menu.children.length]);
         }
+        
+        sidebarContent.appendChild(menu);
+        sidebar.appendChild(sidebarContent);
 
-        sidebar.appendChild(menu);
-
-        var sidebarContent = sidebar.querySelector('ul');
-        var sidebarContentHeight = 0;
 
         // remove whitespace between elements to prevent list spacing issues
         sidebarContent.innerHTML = sidebarContent.innerHTML.replace(
