a61458419fe97029cc9d1c198bd313dd6af548b6
Paul Slaughter <pslaughter@gitlab.com>
2020-04-28 19:38:37 -0500

    Remove old toc handling in docs
    - Will be replaced by Vue component

--- content/assets/javascripts/docs.js
+++ content/assets/javascripts/docs.js
@@ -36,7 +36,6 @@ function toggleNavigation() {
 // move document nav to sidebar
 (function() {
   var timeofday = document.getElementById('timeofday');
-  var tocList = document.querySelector('.js-article-content > ul#markdown-toc');
   var main = document.querySelector('.js-main-wrapper');
 
   // Set timeofday var depending on the time //
@@ -55,93 +54,6 @@ function toggleNavigation() {
 
     if (hour >= 16) {
       timeofday.innerHTML = 'evening';
-    }
-  }
-
-  // if the document has a top level nav
-  if (tocList) {
-    // append to the sidebar
-    var sidebar = document.getElementById('doc-nav');
-
-    if (sidebar) {
-      // if there are items
-      if (tocList.children.length >= 1) {
-        var menu = tocList;
-        $(tocList).addClass('nav nav-pills flex-column');
-        $(tocList).find('ul').addClass('nav nav-pills flex-column');
-        $(tocList).find('a').addClass('nav-link');
-
-        // grab the h1's li anchor text
-        var title = document.createElement('h4');
-        title.innerHTML = 'On this page:';
-
-        // add the text as a title
-        menu.insertBefore(title, menu.children[0]);
-
-        var hasHelpSection = document.getElementById('help-and-feedback');
-
-        // Adds help section anchor to the ToC sidebar
-        if(hasHelpSection) {
-          var listItem = document.createElement('li');
-          var anchor = document.createElement('a');
-          var separator = document.createElement('hr');
-
-          anchor.className = 'nav-link';
-          anchor.innerHTML = 'Help and feedback';
-          anchor.setAttribute('href', '#help-and-feedback');
-          listItem.appendChild(anchor);
-
-          menu.insertBefore(separator, menu.children[menu.children.length]);
-          menu.insertBefore(listItem, menu.children[menu.children.length]);
-        }
-
-        sidebar.appendChild(menu);
-
-        var sidebarContent = sidebar.querySelector('ul');
-        var sidebarContentHeight = 0;
-
-        // remove whitespace between elements to prevent list spacing issues
-        sidebarContent.innerHTML = sidebarContent.innerHTML.replace(
-          new RegExp('>[s\r\n]+<', 'g'),
-          '><'
-        );
-
-        // When we scroll down to the bottom, we don't want the footer covering
-        // the TOC list (sticky behavior)
-        document.addEventListener(
-          'scroll',
-          function() {
-            // Wait a cycle for the dimensions to kick in
-            if (!sidebarContentHeight) {
-              sidebarContentHeight =
-                sidebarContent.getBoundingClientRect().height + 55;
-            }
-
-            var isTouchingBottom = false;
-            if (window.innerWidth >= NAV_INLINE_BREAKPOINT) {
-              isTouchingBottom =
-                window.scrollY + sidebarContentHeight >= main.offsetHeight;
-            }
-
-            if (isTouchingBottom) {
-              sidebarContent.style.top =
-                main.offsetHeight -
-                (window.scrollY + sidebarContentHeight) +
-                'px';
-            } else {
-              sidebarContent.style.top = '';
-            }
-          },
-          { passive: true }
-        );
-      }
-    }
-
-    // main content has-toc
-    if (main && main.classList) {
-      main.classList.add('has-toc');
-    } else {
-      main.className += ' has-toc';
     }
   }
 
