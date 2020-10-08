f494d1179980f1c116929f7e1c7196a99b4fcc9e
Achilleas Pipinellis <axil@gitlab.com>
2020-10-08 08:35:09 +0200

    Add CSP headers

---
+++ content/assets/javascripts/docsearch.js
@@ -0,0 +1,19 @@
+---
+version: 1
+---
+
+var search = docsearch({
+  apiKey: 'ce1690e1421303458a1fcbea0cc4a927',
+  indexName: 'gitlab',
+  inputSelector: '.docsearch',
+  algoliaOptions: {
+    // Filter by tags as described in https://github.com/algolia/docsearch-configs/blob/master/configs/gitlab.json
+    'filters': "tags:gitlab<score=4> OR tags:omnibus<score=3> OR tags:runner<score=2> OR tags:charts<score=1>",
+    // Number of results shown in the search dropdown
+    'hitsPerPage': 10
+  },
+  debug: false, // Set debug to true if you want to inspect the dropdown
+  autocompleteOptions: {
+      'autoselect': false
+    }
+});
