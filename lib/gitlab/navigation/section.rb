3aa6e6e9e618cb68eb8557df744722b188f795bd
Vasilii Iakliushin <viakliushin@gitlab.com>
2020-11-06 11:35:18 +0100

    Extract Navigation class to manage layout variables
    Contributes to https://gitlab.com/gitlab-org/gitlab-docs/-/issues/879

---
+++ lib/gitlab/navigation/section.rb
@@ -0,0 +1,37 @@
+module Gitlab
+  class Navigation
+    class Section
+      def initialize(section)
+        @section = section
+      end
+
+      def title
+        section[:section_title]
+      end
+
+      def ee_only?
+        section[:ee_only]
+      end
+
+      def ee_tier
+        section[:ee_tier]
+      end
+
+      def url
+        section[:section_url]
+      end
+
+      def has_children?
+        !children.empty?
+      end
+
+      def children
+        @children ||= section.fetch(:section_categories, []).map { |cat| Category.new(cat) }
+      end
+
+      private
+
+      attr_reader :section
+    end
+  end
+end
