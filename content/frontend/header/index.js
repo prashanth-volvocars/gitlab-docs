b89adfcf9554f054762883639b19d877e6e895b0
Evan Read <eread@gitlab.com>
2021-02-08 01:30:38 +0000

    Revert "Merge branch 'header-redo' into 'master'"
    This reverts merge request !1486

--- content/frontend/header/index.js
+++ content/frontend/header/index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const navBar = document.querySelector('.header');
+  const navToggle = document.querySelector('.nav-toggle');
   const mobileToggle = document.querySelector('.mobile-nav-toggle');
   const classes = [document.querySelector('.main'), document.querySelector('.nav-wrapper')];
+  navToggle.addEventListener('click', () => navBar.classList.toggle('active'));
   if (!mobileToggle) {
     return;
   }
