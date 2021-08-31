58bbc10e198c686372a1f69dc0078c1e6c300b76
Evan Read <eread@gitlab.com>
2021-08-31 15:32:21 +1000

    No longer collapse lists when version history is multi-line

--- lib/filters/introduced_in.rb
+++ lib/filters/introduced_in.rb
@@ -1,11 +1,10 @@
-# Adapted from the admonition code on http://nanoc.ws/
+# Adapted from the admonition code on https://nanoc.app
 class IntroducedInFilter < Nanoc::Filter
   identifier :introduced_in
 
   def run(content, params = {})
     # `#dup` is necessary because `.fragment` modifies the incoming string. Ew!
     # See https://github.com/sparklemotion/nokogiri/issues/1077
-    @incremental_id = 0
     doc = Nokogiri::HTML.fragment(content.dup)
     doc.css('blockquote').each do |blockquote|
       content = blockquote.inner_html
@@ -18,7 +17,7 @@ class IntroducedInFilter < Nanoc::Filter
       # - "renamed <optional text> in"
       # - "changed <optional text> in"
       # ...followed by "GitLab"
-      next if content !~ /(<a href="[^"]+">)?(introduced|(re)?moved|changed|deprecated|renamed|recommended)(<\/a>)?(.*)? (in|to).*GitLab/mi
+      next unless content.match?(%r{(<a href="[^"]+">)?(introduced|(re)?moved|changed|deprecated|renamed|recommended)(</a>)?(.*)? (in|to).*GitLab}mi)
 
       new_content = generate(content)
       blockquote.replace(new_content)
@@ -27,15 +26,6 @@ class IntroducedInFilter < Nanoc::Filter
   end
 
   def generate(content)
-    @incremental_id += 1
-    # If the content is a list of items, collapse the content.
-    if content =~ /<ul>/i
-      %(<div class="introduced-in mb-3">Version history) +
-        %(<button class="text-expander" type="button" data-toggle="collapse" data-target="#release_version_notes_#{@incremental_id}" aria-expanded="false" aria-controls="release_version_notes_#{@incremental_id}" aria-label="Version history">) +
-        %(</button>) +
-        %(<div class="introduced-in-content collapse" id="release_version_notes_#{@incremental_id}">#{content}</div></div>)
-    else
-      %(<div class="introduced-in">#{content}</div>)
-    end
+    %(<div class="introduced-in">#{content}</div>)
   end
 end
