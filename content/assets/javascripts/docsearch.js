82346651e8513ff5345be158d69bbbc579344388
Sarah German <sgerman@gitlab.com>
2022-06-09 15:57:16 +0000

    Facet results by version in Algolia search

--- content/assets/javascripts/docsearch.js
+++ content/assets/javascripts/docsearch.js
@@ -1,8 +1,13 @@
 ---
-version: 3
+version: 4
 ---
 
 document.addEventListener('DOMContentLoaded', () => {
+  let version = "main"
+  if (document.querySelector('meta[name="docsearch:version"]').content.length > 0) {
+    version = document.querySelector('meta[name="docsearch:version"]').content;
+  }
+
   // eslint-disable-next-line no-undef
   docsearch({
     apiKey: '89b85ffae982a7f1adeeed4a90bb0ab1',
@@ -10,6 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
     container: '#docsearch',
     appId: "3PNCFOU757",
     placeholder: 'Search the docs',
+    searchParameters: {
+      facetFilters: [`version:${version}`],
+    },
     resultsFooterComponent({ state }) {
       return {
         type: "a",
